d7305e93bc4a119d73ab0cfd89541c883dd15afb
LOGESHSAMINATHAN <23124027@srcas.ac.in>
2024-12-07 12:06:59 +0530

    push

---
+++ logesh.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class palin{  
+ public static void main(String args[]){  
+  int r,sum=0,temp;    
+  Scanner obj = new Scanner(System.in);
+  System.out.println("enter the number:"); 
+  int n=obj.nextInt();
+  temp=n;    
+  while(n>0){    
+   r=n%10;  //getting remainder  
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");    
+}  
+}
